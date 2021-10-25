c7d9c47a065fc0523e28c21700d5a733c05fe5e1
Guilherme <56496944+glhrmarques@users.noreply.github.com>
2021-10-25 09:18:05 -0300

    Create conditions004.js

---
+++ conditions004.js
@@ -0,0 +1,11 @@
+// Practicing Condition with if / else if 
+
+var spotlight = 'green'
+
+if ( spotlight == 'green') {
+    console.log('You can go')
+} else if ( spotlight == 'red') {
+    console.log('Stop')
+} else (
+    console.log('be careful')
+)
