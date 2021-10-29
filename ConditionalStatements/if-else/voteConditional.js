b865a1e94f492959e8606c580f0b00d366c5f9b4
Guilherme <56496944+glhrmarques@users.noreply.github.com>
2021-10-29 14:48:59 -0300

    Create voteConditional.js

---
+++ ConditionalStatements/if-else/voteConditional.js
@@ -0,0 +1,9 @@
+var idade = 17
+
+if (idade > 18 ) {
+    console.log('Your must vote')
+} else if ( idade > 16 || idade > 65 ) {
+    console.log('Your vote is opcional')
+} else {
+    console.log('Your do not need to vote')
+}
